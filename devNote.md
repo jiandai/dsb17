0eb3cdf2266ff05f2e465e5306ce0e77bfaa0f50
Jian Dai <daij12@gene.com>
2017-02-23 16:28:48 -0800

    non-pixel (meta) data extraction

--- devNote.md
+++ devNote.md
@@ -1,1 +1,16 @@
 make the dir at 1/13/2017 12:32:17 PM
+
+ref on pydicom:
+http://pydicom.readthedocs.io/en/stable/getting_started.html
+
+note for 2/22 DL meeting:
+
+imageJ
+nifity
+
+osirix
+osirix
+
+
+Convolutional layer:
+Phi^I_ab = \sum_x \sum_y W^{IJ}_{ab,xy} phi^J_{xy}+ b^I
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#%%
 # -*- coding: utf-8 -*-
 """
 version 20170116 by Jian:
@@ -8,21 +9,267 @@ ref:
 
 version 20170131 by Jian: revisit
 version 20170206 by Jian: revisit
+
 version 20170221 by Jian: more on visual 
-to-do read scan together, normalization, packaging, OOing
+version 20170222 by Jian: batch process, metadata DB
+version 20170223 by Jian: metadata at patient level, slide level, and unhashable types
+
+to-do normalization, packaging, OOing
 """
 
-#Working dir: 
-#%%
-#PWD='C:\\Users\\daij12\\Documents\\Analysis\\ML\\kaggle\\data-science-bowl-2017'
+
+
 PWD='..'
-import os
-#%%
 INPUT_FOLDER=PWD+'/sample_images/'
-#%%
+import os
 patients = os.listdir(INPUT_FOLDER)
-#%%
+print(patients)
+
 import dicom
+allScan=[[dicom.read_file(INPUT_FOLDER+pat+'/'+s) for s in os.listdir(INPUT_FOLDER+pat)] for pat in patients]
+
+
+#%%
+fstfst = allScan[0][0]
+#%%
+scdfst = allScan[1][0]
+
+#%%
+print(fstfst.dir())
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+
+
+#%%
+rec=dict()
+pos=dict()
+for name in fstfst.dir():
+    if name!='PixelData':
+        print(name+'[')
+        print(fstfst.data_element(name).tag)
+        print(fstfst.data_element(name).name)
+        print(fstfst.data_element(name).VR)
+        print(fstfst.data_element(name).VM)
+        print(fstfst.data_element(name).value)
+        print(']')
+        rec[name] = fstfst.data_element(name).value
+        pos[name] = fstfst.data_element(name).tag
+    else:
+        print(type(fstfst.PixelData))
+
+#%%
+import pandas as pd
+#%%
+pd.DataFrame([{'a':1,'b':2}]).append(pd.DataFrame([{'a':4,'b':5}]))
+#%%
+pd.DataFrame([{'b':2}]).append(pd.DataFrame([{'a':'bb','b':5}]))
+#%%
+pd.DataFrame([{'a':'aa','b':2}]).append(pd.DataFrame([{'b':5}]))
+#%%
+pd.DataFrame([rec]).append(pd.DataFrame([rec])).shape
+#%%
+metaDB0 = pd.DataFrame()
+colDB0 = pd.DataFrame()
+for sld in allScan[0]:
+    red=dict()
+    for name in sld.dir():
+        if name!='PixelData':
+            red[name] = sld.data_element(name).value
+            colDB0 = colDB0.append(pd.DataFrame([{'col':name,'pos':sld.data_element(name).tag}]))
+    metaDB0 = metaDB0.append(pd.DataFrame([red]))
+#%%
+metaDB0.shape
+#%%
+colDB0
+#%%
+colDB0.drop_duplicates().shape
+#%%
+bs = set(fstfst.dir())
+allCol=bs
+cnt = 0
+metaDB = pd.DataFrame()
+colDB=pd.DataFrame()
+for patScan in allScan:
+    for sld in patScan:
+        cnt += 1
+        s=set(sld.dir())
+        diff=s.symmetric_difference(bs)
+        rec=dict()
+        for name in sld.dir():
+            if name != 'PixelData':
+                rec[name] = sld.data_element(name).value
+                colDB = colDB.append(pd.DataFrame([{'col':name,'pos':sld.data_element(name).tag}]))
+        metaDB = metaDB.append(pd.DataFrame([rec]))
+        if len(diff)>0:
+            print(sld.PatientID)
+            print(sld.InstanceNumber)
+            allCol=allCol.union(s)
+            #print(diff)
+        #for it in sld.dir():
+        #    print(it)
+#%%
+print(cnt)
+#%%
+metaDB.shape
+#%%
+colDB.shape
+#%%
+colDB.drop_duplicates().shape
+#%%
+colDB1=colDB.drop_duplicates()
+#%%
+colDB1 = colDB1.sort_values(['pos'])
+
+
+
+#%%
+'''
+Here we try two different ways to define the preferred order of the meta data fields
+'''
+#%%
+preferredOrder = colDB1.col
+#%%
+''' 
+preferredOrder = ['PatientID',
+'PatientName',
+'InstanceNumber',
+'AcquisitionNumber',
+'BitsAllocated',
+'BitsStored',
+'BurnedInAnnotation',
+'Columns',
+'Rows',
+'FrameOfReferenceUID',
+'HighBit',
+'ImageOrientationPatient',
+'ImagePositionPatient',
+'KVP',
+'LargestImagePixelValue',
+'LongitudinalTemporalInformationModified',
+'Modality',
+'PatientBirthDate',
+'PatientOrientation',
+'PhotometricInterpretation',
+'PixelAspectRatio',
+'PixelPaddingValue',
+'PixelRepresentation',
+'PixelSpacing',
+'PlanarConfiguration',
+'PositionReferenceIndicator',
+'RescaleIntercept',
+'RescaleSlope',
+'RescaleType',
+'SOPClassUID',
+'SOPInstanceUID',
+'SamplesPerPixel',
+'SeriesDescription',
+'SeriesInstanceUID',
+'SeriesNumber',
+'SliceLocation',
+'SmallestImagePixelValue',
+'SpecificCharacterSet',
+'StudyInstanceUID',
+'WindowWidth',
+'WindowCenter',
+'WindowCenterWidthExplanation']
+# minus 'PixelData',
+'''
+#%%
+metaDB = metaDB [list(preferredOrder)]
+#%% 
+metaDB = metaDB.sort_values(['PatientID','InstanceNumber'])
+#%%
+#metaDB.to_csv('sample_images_dicom_metadata.csv',index=False) # use manually defined preferred order
+metaDB.to_csv('sample_images_dicom_metadata_v1.2.csv',index=False) # use automatically created preferred order by dicom tags
+
+
+
+#%%
+# some series cannot use unique method due the type is not HASHABL
+# lesson is DF may not be the best data structure for some operations to store DICOM metadata
+#%%
+N=metaDB.PatientID.unique().shape[0]
+#%%
+patLevelCol=dict()
+slideLevelCol=dict()
+unHashableCol=dict()
+#%%
+for c in metaDB.columns[:]:    
+    try:
+        cnt=metaDB[c].unique().shape[0]
+        print(cnt)
+        if cnt>N:
+            slideLevelCol[c] = cnt
+            print(len(slideLevelCol))
+        else:
+            patLevelCol[c] = cnt
+            print(len(patLevelCol))           
+    except TypeError:
+        unHashableCol[c] = type(metaDB[c].iloc[0])
+        #print(c+'='+metaDB[c].apply(lambda x:str(x)).unique().shape[0])
+
+#%%
+# Put patient level tags in one dataset, and slide level + unhashables in another one
+metaDB[[k for k,v in patLevelCol.items()]].drop_duplicates().shape
+#%%
+patLevelDB = metaDB[[k for k,v in patLevelCol.items()]].drop_duplicates()
+#%%
+slideLevelDB = metaDB[['PatientID']+[k for k,v in slideLevelCol.items()] + [k for k,v in unHashableCol.items()]]
+#%%
+patLevelDB.columns.intersection(slideLevelDB.columns) # PatientID only
+
+#%%
+# Check the dup in patient level DB
+patLevelDB.groupby(['PatientID']).count()
+#%%
+patLevelDB.groupby(['PatientID']).size()
+#%%
+patLevelDB[patLevelDB.PatientID == '0bd0e3056cbf23a1cb7f0f0b18446068'].shape
+patLevelDB[patLevelDB.PatientID == '0bd0e3056cbf23a1cb7f0f0b18446068'].to_csv('dup.csv')
+patLevelDB.drop('AcquisitionNumber',1).drop_duplicates().shape
+# So the root of the dup is for one particular patient having 5 "AcquisitionNumber" in ths slides
+
+metaDB[metaDB.PatientID == '0bd0e3056cbf23a1cb7f0f0b18446068'].shape
+dupPat = metaDB[metaDB.PatientID == '0bd0e3056cbf23a1cb7f0f0b18446068']
+dupPat.shape
+dupPat.groupby(['AcquisitionNumber']).size()
+'''
+AcquisitionNumber
+1    240
+2      5
+3      5
+4     10
+5     20
+dtype: int64
+'''
+# Move /w Instance number
+dupPat[['InstanceNumber','AcquisitionNumber']]
+
+
+# output:
+
+
+'''
+=> Pixel data
+most interested fields:
+Instance Number
+Image Position
+Image Orientation
+Slice Location
+Pixel Spacing 
+Rescale Intercept
+Rescale Slope
+
+'''
+
+quit()
+
 import matplotlib.pyplot as plt
 
 
